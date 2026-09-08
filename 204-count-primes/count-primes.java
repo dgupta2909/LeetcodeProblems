fb319ff6c4ac436a95a1cc1d01d7190f21268ae3
Devanshu Gupta <dgupta290906@gmail.com>
2026-09-08 20:55:37 +0530

    Time: 759 ms (14.78%) | Memory: 80.2 MB (5.46%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -3,48 +3,23 @@ class Solution {
     public int countPrimes(int n) {
 
         if (n <= 1) {
-            return 0;
+       return 0;
         }
-
         int count = 0;
-
         boolean[] isprime = new boolean[n];
-
         Arrays.fill(isprime, true);
-
-        //traverse untill sqrt(n)
         for (int i = 2; i * i < n; i++) {
-
             if (isprime[i] == true) {
-
-                // i's multiples can't be a prime number
                 for (int j = i * i; j < n; j = j + i) {
-
                     isprime[j] = false;
-
                 }
-
             }
-
         }
-
-        //counting prime
         for (int i = 2; i < n; i++) {
-
             if (isprime[i] == true) {
-
                 count++;
-
             }
-
         }
-
         return count;
-
     }
-
 }
-
-//Please click the ⬆️ UPARROW ⬆️ button in the bottom left corner of your screen
-
-//That means you Like👍the solution
